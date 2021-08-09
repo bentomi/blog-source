078cc0d4baefd67bf045ad6c9c146821e166469c
Tamas Benkoe <tamas.benkoe@data42.de>
2021-08-09 08:33:31 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.8.0"]
-                           [ring/ring-devel "1.6.1"]
-                           [compojure "1.6.0"]
-                           [ring-server "0.4.0"]
+            :dependencies [[org.clojure/clojure "1.10.3"]
+                           [ring/ring-devel "1.9.4"]
+                           [compojure "1.6.2"]
+                           [ring-server "0.5.0"]
                            [cryogen-markdown "0.1.6"]
                            [cryogen-core "0.1.56"]]
-            :plugins [[lein-ring "0.9.7"]]
+            :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
                    :handler cryogen.server/handler})
